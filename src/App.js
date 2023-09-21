233c5e8b7deb13e4c8d734c58da7559d753c9287
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-09-21 17:09:09 +0200

    Create state isLoading, and render jsx if isLoading

--- src/App.js
+++ src/App.js
@@ -35,13 +35,14 @@ function formatDay(dateStr) {
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { location: "lisbon" };
+    this.state = { location: "lisbon", isLoading: false };
     this.fetchWeather = this.fetchWeather.bind(this);
   }
 
   async fetchWeather() {
     try {
-      console.log(this);
+      this.setState({ isLoading: true });
+
       // 1) Getting location (geocoding)
       const geoRes = await fetch(
         `https://geocoding-api.open-meteo.com/v1/search?name=${this.state.location}`
@@ -63,6 +64,8 @@ class App extends React.Component {
       console.log(weatherData.daily);
     } catch (err) {
       console.err(err);
+    } finally {
+      this.setState({ isLoading: false });
     }
   }
 
@@ -79,6 +82,7 @@ class App extends React.Component {
           />
         </div>
         <button onClick={this.fetchWeather}>Get Weather</button>
+        {this.state.isLoading && <p className="loader">Loading...</p>}
       </div>
     );
   }
