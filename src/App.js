b49d4f1516983ac094e1d88ade55d2ce72a6d964
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-09-21 13:11:00 +0200

    Add state with the location

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { location: "lisbon" };
+  }
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
         <div>
-          <input type="text" placeholder="Search from location..." />
+          <input
+            type="text"
+            placeholder="Search from location..."
+            value={this.state.location}
+            onChange={(e) => this.setState({ location: e.target.value })}
+          />
         </div>
+        <button>Get Weather</button>
       </div>
     );
   }
